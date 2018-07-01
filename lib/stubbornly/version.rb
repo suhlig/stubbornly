d000fb8100608497fb31cb7461bffba838d61357
Steffen Uhlig <steffen@familie-uhlig.net>
2018-07-01 20:06:28 +0200

    Disable Rubocop rule Style/RedundantFreeze for Travis
    For some reason Rubocop complains about this rule when run on Travis.

--- lib/stubbornly/version.rb
+++ lib/stubbornly/version.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Stubbornly
-  VERSION = '0.2.0'
+  # rubocop:disable Style/RedundantFreeze
+  VERSION = '0.2.0'.freeze
+  # rubocop:enable Style/RedundantFreeze
 end
