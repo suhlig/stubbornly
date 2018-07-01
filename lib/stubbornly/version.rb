618ae0f30c4df40c9a2cceba419ab9a653d87103
Steffen Uhlig <steffen@familie-uhlig.net>
2018-07-01 20:21:28 +0200

    Revert "Disable Rubocop rule Style/RedundantFreeze for Travis"
    This reverts commit d000fb8100608497fb31cb7461bffba838d61357. It did not
    help.

--- lib/stubbornly/version.rb
+++ lib/stubbornly/version.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class Stubbornly
-  # rubocop:disable Style/RedundantFreeze
-  VERSION = '0.2.0'.freeze
-  # rubocop:enable Style/RedundantFreeze
+  VERSION = '0.2.0'
 end
